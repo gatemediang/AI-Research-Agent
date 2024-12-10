d958f8107738b56320a1692e5e2452f56dd8038e
Tunji Ologun <estolog@gmail.com>
2024-12-10 14:55:39 +0000

    Update constants.py
    rem

--- constants.py
+++ constants.py
@@ -1,15 +1,1 @@
-# from tosinmail
-API_KEY = "AIzaSyAN5JS8B1584IBGd4VlzXvtsKykTFZI62M"
 
-# ... other constants ...
-parse_key = "llx-bLmAiV4yXtdRpEjTBAq7TZa4TFuJmQmOcMoRUTHXzbLmmdd5"  # Add this line
-# ... other constants ...
-
-user_details = {
-    "username": "wisdom",
-    "favourite_movies": "dc comics, marvel studios",
-    "age": "30",
-    "married": "true",
-    "BMI": "obese",
-    "health_status": "okay",
-}
